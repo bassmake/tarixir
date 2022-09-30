872c3d74b9efabff44e78f399f0fc44a5df4eb02
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2022-09-30 05:19:27 +0200

    init

---
+++ lib/tarixir.ex
@@ -0,0 +1,18 @@
+defmodule Tarixir do
+  @moduledoc """
+  Documentation for `Tarixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tarixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tarixir_test.exs
@@ -0,0 +1,8 @@
+defmodule TarixirTest do
+  use ExUnit.Case
+  doctest Tarixir
+
+  test "greets the world" do
+    assert Tarixir.hello() == :world
+  end
+end
